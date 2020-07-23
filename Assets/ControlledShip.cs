c95b2ebd5296893090b1ca29947398884574e0d5
floatPond <samcrosby@live.co.uk>
2020-07-23 17:06:53 +0100

    SC - Updated project

---
+++ Assets/ControlledShip.cs
@@ -0,0 +1,178 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BNG;
+using TMPro;
+public class ControlledShip : MonoBehaviour
+{
+    //Text
+    public TextMeshProUGUI textSpeed,textPitch,textRoll,X,Z,d2g;
+
+    //For input
+    public GameObject Player;
+    InputBridge ib;
+
+    //Controls
+    float degreePitch, degreeRoll, degreeYaw, degreeAcceleration,degreeX,degreeZ;
+    bool lockedPitch, lockedRoll, lockedAcceleration;
+    //Ship
+    public float currentSpeed;
+    public float maximumSpeed = 100;
+    public float distanceToGround;
+    Vector3 CraftSpawnP;
+    Quaternion CraftSpawnR;
+
+    void Start()
+    {
+        ib = Player.GetComponent<InputBridge>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Ray
+        Ray ray = new Ray(transform.position, Vector3.down);
+        RaycastHit hitInfo;
+
+        if (Physics.Raycast(ray, out hitInfo, 10000))
+        {
+            //Debug.Log("Hit "+hitInfo.transform.gameObject.name+","+Vector3.Distance(ray.origin, hitInfo.point));
+            distanceToGround = Vector3.Distance(ray.origin, hitInfo.point);
+            d2g.text = distanceToGround.ToString("F2");
+            //Debug.DrawLine(ray.origin, hitInfo.point, Color.red,2.0f);
+        }
+        else
+        {
+            Debug.Log("Miss!");
+            //Debug.DrawLine(ray.origin, ray.origin + ray.direction * 100, Color.green, 2.0f);
+        }
+
+        //Locking
+        if (ib.LeftGrip > 0.5)
+        {
+            lockedPitch = true;
+            lockedRoll = true;
+        }
+        else
+        {
+            lockedPitch = false;
+            lockedRoll = false;
+        }
+        if (ib.RightGrip > 0.5)
+        {
+            lockedAcceleration = true;
+        }
+        else
+        {
+            lockedAcceleration = false;
+        }
+
+        //Calculating rotation and speed
+        //Get pitch
+        if (!lockedPitch)
+        { 
+            degreePitch = ib.LeftControllerRotation.x;
+            degreePitch = Deadzone(degreePitch, 0.08f);
+        }
+        textPitch.text = "Pitch: " + degreePitch.ToString("F2");
+
+        //Pitch text colour
+        if (ib.LeftControllerRotation.x > 0.05)
+        {
+            textPitch.color = Color.green;
+        }
+        else if (ib.LeftControllerRotation.x < 0.05)
+        {
+            textPitch.color = Color.red;
+        }
+        else
+        {
+            textPitch.color = Color.white;
+        }
+
+        //Get roll
+        if (!lockedRoll)
+        { 
+            degreeRoll = ib.LeftControllerRotation.z;
+            degreeRoll = Deadzone(degreeRoll, 0.08f);
+        }
+        textRoll.text = "Roll: " + degreeRoll.ToString("F2");
+        //Get acceleration
+        degreeAcceleration = ib.RightControllerRotation.x;
+        degreeAcceleration = Deadzone(degreeAcceleration, 0.1f);
+
+        
+        transform.Rotate(Vector3.left * degreePitch * 33f * Time.deltaTime, Space.Self); //Pitch pp and down
+        transform.Rotate(Vector3.back * degreeRoll * 100f * Time.deltaTime, Space.Self); //Roll left and right
+
+        //Accelerate
+        if (!lockedAcceleration)
+        { 
+            if (degreeAcceleration > 0)
+            {
+                currentSpeed += degreeAcceleration * 5 * Time.deltaTime;
+            }
+            else //Breaking
+            {
+                currentSpeed += degreeAcceleration * 10 * Time.deltaTime;
+            }
+        }
+        
+        //Speed
+        if (currentSpeed < 0)
+            currentSpeed = 0;
+        if (currentSpeed > maximumSpeed)
+            currentSpeed = maximumSpeed;
+
+        //Buttons
+        if (ib.AButtonDown == true)
+            ResetShip();
+
+        //Update speed
+        transform.Translate(Vector3.forward * (currentSpeed) * Time.deltaTime);
+        textSpeed.text = "Speed: " + currentSpeed.ToString("F1");
+
+        //Update X and Z text
+        degreeX = transform.eulerAngles.x;
+        if (degreeX > 270)
+        {
+            degreeX = degreeX - 360;
+            degreeX *= -1;
+        }
+        degreeZ = transform.eulerAngles.z;
+        if (degreeZ > 270)
+        {
+            degreeZ = degreeZ - 360;
+            degreeZ *= -1;
+        }
+
+        X.text = "X:" + degreeX.ToString("F2");
+        Z.text = "Z:" + degreeZ.ToString("F2");
+
+    }
+
+    void ResetShip()
+    {
+        transform.position = CraftSpawnP;
+        transform.rotation = CraftSpawnR;
+        currentSpeed = 0;
+    }
+    float Deadzone (float number, float extent)
+    {
+        if (number > extent) //e.g 0.6 > 0.5
+        {
+            number -= extent;
+            return number;
+        }
+        else if (number < (extent *= -1))
+        {
+            number += extent;
+            return number;
+        }
+        else
+        {
+            number = 0;
+            return number;
+        }
+    }
+}
---
+++ Assets/InputShower.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using BNG;
+using TMPro;
+
+public class InputShower : MonoBehaviour
+{
+    public GameObject Player;
+    public GameObject cube;
+    Vector3 CraftSpawnP;
+    Quaternion CraftSpawnR;
+    Quaternion CubeSpawnR;
+
+    InputBridge ib;
+    public TextMeshProUGUI LCP, LCR, RCP, RCR, cSpeed, cRotation;
+    bool isJoystickEnabled;
+
+    //Controller angles
+    float degreePitch, degreeRoll, degreeYaw,degreeAcceleration;
+
+    //Ship
+
+    public GameObject craft;
+    public float currentSpeed;
+
+    void Start()
+    {
+        ib = Player.GetComponent<InputBridge>();
+        CraftSpawnP = craft.transform.position;
+        CraftSpawnR = craft.transform.rotation;
+        CubeSpawnR = cube.transform.rotation;
+
+        //Rotations - default is when controller is facing forward, as per normal holding
+        //w is ?, defaults at 1
+        //x is
+        //y is ?, default is 0. Move controller left=+. Move controller right = -
+        //z is roll, defaults at 0
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Get pitch
+        degreePitch = ib.LeftControllerRotation.x;
+        //Get roll
+        degreeRoll = ib.LeftControllerRotation.z;
+        //Get yaw
+        //Not currently used
+
+        //Get acceleration
+        degreeAcceleration = ib.RightControllerRotation.x;
+
+        LCP.text = "LCP-x:" + ib.LeftControllerPosition.x.ToString("F2")+",y:"+ib.LeftControllerPosition.y.ToString("F2") + ",z:" + ib.LeftControllerPosition.z.ToString("F2");
+        RCP.text = "RCP-x:" + ib.RightControllerPosition.x.ToString("F2") + ",y:" + ib.RightControllerPosition.y.ToString("F2") + ",z:" + ib.RightControllerPosition.z.ToString("F2");
+
+        LCR.text = "LCR-w:" + ib.LeftControllerRotation.w.ToString("F2") + ",x:" + ib.LeftControllerRotation.x.ToString("F2") + ",y:" + ib.LeftControllerRotation.y.ToString("F2") + ",z:" + ib.LeftControllerRotation.z.ToString("F2");
+        RCR.text = "RCR-w:" + ib.RightControllerRotation.w.ToString("F2") + ",x:" + ib.RightControllerRotation.x.ToString("F2") + ",y:" + ib.RightControllerRotation.y.ToString("F2") + ",z:" + ib.RightControllerRotation.z.ToString("F2");
+        cRotation.text = "Pitch:" + (degreePitch).ToString("F2") + ",Roll:" + (degreeRoll).ToString("F2");
+        cSpeed.text = "Speed: " + (currentSpeed).ToString("F2");
+
+        
+        if (!isJoystickEnabled)
+        {
+            //Pitch craft //Vector3.left pitches craft
+            if ((degreePitch > 0.05) || (degreePitch < -0.05))
+                craft.transform.Rotate(Vector3.left * degreePitch * 100f * Time.deltaTime, Space.Self);
+            //Roll craft
+            if ((degreeRoll > 0.08) || (degreeRoll < -0.08))
+                craft.transform.Rotate(Vector3.back * degreeRoll * 25f *Time.deltaTime, Space.Self);
+            
+            //Accelerate
+            if ((degreeAcceleration > 0.05)|| (degreeAcceleration < -0.05))
+            { 
+                if (degreeAcceleration > 0)
+                { 
+                currentSpeed += degreeAcceleration * Time.deltaTime;
+                }
+                else //Breaking
+                {
+                    currentSpeed += degreeAcceleration * 2 * Time.deltaTime;
+                }
+            }
+            //Speed
+            if (currentSpeed < 0)
+                   currentSpeed = 0;
+               if (currentSpeed > 30)
+                   currentSpeed = 30;
+                //craft.transform.Translate(Vector3.forward * (degreeAcceleration) * 10f * Time.deltaTime);
+            
+
+            //Cube
+            //cube.transform.Rotate(Vector3.left * ib.LeftControllerRotation.x * 100f * Time.deltaTime, Space.Self);
+            //cube.transform.Rotate(Vector3.down * ib.LeftControllerRotation.y * 100f * Time.deltaTime, Space.Self);
+            //cube.transform.Rotate(Vector3.back * ib.LeftControllerRotation.z * 100f * Time.deltaTime, Space.Self);
+
+        }
+        else
+        {
+            craft.transform.Rotate(Vector3.back * ib.LeftThumbstickAxis.x * 100 * Time.deltaTime, Space.Self);
+            craft.transform.Rotate(Vector3.right * ib.LeftThumbstickAxis.y * 100f * Time.deltaTime, Space.Self);
+            craft.transform.Translate(Vector3.forward * (ib.RightThumbstickAxis.y) * 10f * Time.deltaTime);
+        }
+        if (ib.AButtonDown == true)
+            ResetCraftPosition();
+        if (ib.BButtonDown == true)
+            isJoystickEnabled = !isJoystickEnabled;
+        craft.transform.Translate(Vector3.forward * (currentSpeed) * Time.deltaTime);
+    }
+    void ResetCraftPosition()
+    {
+        craft.transform.position = CraftSpawnP;
+        craft.transform.rotation = CraftSpawnR;
+        cube.transform.rotation = CubeSpawnR;
+        currentSpeed = 0;
+    }
+}
